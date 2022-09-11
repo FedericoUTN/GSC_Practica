92bc36ac33cb0a80d2b56c6a30cfc7cb960f2d8d
Federico <hulalaoooo@gmail.com>
2022-09-11 18:25:13 -0300

    ejercicio 1.5

--- typescript/ejercicio1.ts
+++ typescript/ejercicio1.ts
@@ -52,3 +52,12 @@ const elNumeroMasGrande : number = Infinity;
  miembros[0] = Number('12345');
 
  console.log('[Ejercicio 1.4]', miembros);
+
+ //1.5
+
+const secuencia: number[] = Array.from(Array(10).keys());
+const animales: string[] = ['pinguino', 'oso hormiguero', 'zorro', 'jirafa'];
+const cadenasYNumeros: (number|string)[] = [1, 'uno', 2, 'dos', 3, 'tres'];
+const todosMisArreglos: any[] = [secuencia, animales, cadenasYNumeros];
+
+console.log('Ejercicio 1.5', todosMisArreglos);
---
+++ typescript/test.ts
@@ -0,0 +1,3 @@
+let secuencia: number[] = Array.from(Array(10).keys());
+
+console.log('Ejercicio 1.5', secuencia);
