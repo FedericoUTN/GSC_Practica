277fa44716fcf386d497321c7daac6cffb6ee231
Federico <hulalaoooo@gmail.com>
2022-09-11 18:43:17 -0300

    ejercicio 1.6

--- typescript/ejercicio1.ts
+++ typescript/ejercicio1.ts
@@ -61,3 +61,17 @@ const cadenasYNumeros: (number|string)[] = [1, 'uno', 2, 'dos', 3, 'tres'];
 const todosMisArreglos: any[] = [secuencia, animales, cadenasYNumeros];
 
 console.log('Ejercicio 1.5', todosMisArreglos);
+
+//1.6
+
+const elementoInventario: [string, number] = ['tuerca', 11];
+
+// despues lo desestructuramos
+const [nombre, cantidad] = elementoInventario;
+const mensaje = agregarInventario(nombre , cantidad);
+
+console.log('[Ejercicio 1.6]', mensaje);
+
+function agregarInventario(nombre: string, cantidad: number): string {
+return `Se agregaron ${cantidad} ${nombre}s al inventario.`;
+}
