b5afcbac8d215975e1279e40fc54b9ace2c070eb
Federico <hulalaoooo@gmail.com>
2022-09-14 13:37:58 -0300

    tipos ejercicios-3

--- typescript/tipos.ts
+++ typescript/tipos.ts
@@ -95,3 +95,25 @@ return animal.species;
 console.log('[Ejercicio 4.4]',
 `Tenemos un ${interrogateAnimal()} en nuestras manos!`);
 //Restrinja el tipo de ‘valor‘ a ‘string o number‘
+
+
+//3
+
+function padLeft(value: string, padding: number | string): string {
+// si padding es un numero, 
+if(typeof padding === "number"){
+    return `${Array(padding + 1).join(' ')}${value}`;
+}
+else if(typeof padding === "string"){
+    return padding + value;
+}
+else return '';
+}
+
+console.log('[Ejercicio 4.2]', `
+${padLeft('', 0)}
+${padLeft('', '')}
+${padLeft('', '')}
+${padLeft('', '')}
+${padLeft('', '')}
+`);
